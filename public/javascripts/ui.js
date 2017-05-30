7e557784af50a2a955c0037a5364b98ec4ffdecc
gaspard <gaspard@gmail.com>
2017-05-30 14:29:41 +0200

    ui: add a grid

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -1,9 +1,40 @@
 class Ui {
   constructor(canvas) {
-    this.canvas = canvas || document.querySelector('canvas');
+    this.canvas = canvas;
     this.context = canvas.getContext('2d');
+    this.grid();
   }
   tick() {
 
   }
+  // grid
+  grid(context) {
+    this.context.strokeStyle = '#ccc'; // '#ff115f';
+    this.context.lineWidth = 0.5;
+
+    const bw = 400;
+    const bh = 400;
+    //padding around grid
+    const p = 0;
+    //size of canvas
+    const cw = bw + (p*2) + 1;
+    const ch = bh + (p*2) + 1;
+
+    for (var x = 0; x <= bw; x += 50) {
+      this.context.moveTo(0.5 + x + p, p);
+      this.context.lineTo(0.5 + x + p, bh + p);
+    }
+
+    for (var x = 0; x <= bh; x += 50) {
+      this.context.moveTo(p, 0.5 + x + p);
+      this.context.lineTo(bw + p, 0.5 + x + p);
+    }
+
+    this.context.strokeStyle = "black";
+    this.context.stroke();
+  }
 }
+
+window.addEventListener('load', () => {
+  const ui = new Ui(document.querySelector('canvas'))
+}, false);
