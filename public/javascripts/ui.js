bb8bb28aa7e5711e33e29b43734bf7c81cb06520
gaspard <gaspard@gmail.com>
2017-05-30 14:54:00 +0200

    ui: use grid as design element

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -2,32 +2,36 @@ class Ui {
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
+
+    // canvas width, height and unit (in pixels)
+    this.width = 400;
+    this.height = 400;
+    this.unit = 50;
+
     this.grid();
   }
   tick() {
 
   }
   // grid
-  grid(context) {
+  grid() {
     this.context.strokeStyle = '#ccc'; // '#ff115f';
     this.context.lineWidth = 0.5;
 
-    const bw = 400;
-    const bh = 400;
     //padding around grid
     const p = 0;
     //size of canvas
-    const cw = bw + (p*2) + 1;
-    const ch = bh + (p*2) + 1;
+    const cw = this.width + (p*2) + 1;
+    const ch = this.height + (p*2) + 1;
 
-    for (var x = 0; x <= bw; x += 50) {
+    for (var x = 0; x <= this.width; x += this.unit) {
       this.context.moveTo(0.5 + x + p, p);
-      this.context.lineTo(0.5 + x + p, bh + p);
+      this.context.lineTo(0.5 + x + p, this.height + p);
     }
 
-    for (var x = 0; x <= bh; x += 50) {
+    for (var x = 0; x <= this.height; x += this.unit) {
       this.context.moveTo(p, 0.5 + x + p);
-      this.context.lineTo(bw + p, 0.5 + x + p);
+      this.context.lineTo(this.width + p, 0.5 + x + p);
     }
 
     this.context.strokeStyle = "black";
